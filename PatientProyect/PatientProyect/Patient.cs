bf76c6a985303e8a3df4e58d86b5f2b2eec8192b
samucorea <samuel.alejandro100@hotmail.com>
2020-03-26 19:00:50 -0400

    Agregados metodos Patient

---
+++ PatientProyect/PatientProyect/Patient.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatientProyect
+{
+    class Patient
+    {
+        public static string nombre = "Fulano";
+        public static string apellidos = "Mengano";
+        public static List<string> Disorders = new List<string>();
+        public static string[] enfermedades = {"Alcohol Addiction", "Anorexia", "Antisocial", "Anxiety", "Bipolar", "Bulimia", "Dependent (lack of self" +
+                    "confidence" , "Depression", "Kleptomaniac", "Nicotine addiction", "Obsessive-compulsive", "Panic", "Paranoid", "Post-traumatic stress",
+            "Programmer", "Tester",};
+
+        public static string DisorderGet(uint x)
+        {
+            for (int i = 0; i < enfermedades.Length; i++)
+            {
+                long shift = 1 << i;
+                if ((x & shift) > 0)
+                {
+                    return enfermedades[i];
+                }
+
+            }
+            return "";
+        }
+        public static bool hasDisorder(uint disorder)
+        {
+            string checkDisorder = DisorderGet(disorder);
+
+            if (Disorders.Contains(checkDisorder))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool addDisorder(uint x)
+        {
+            string newDisorder = DisorderGet(x);
+
+            if (newDisorder == "")
+            {
+                return false;
+            }
+
+            else if (Disorders.Contains(newDisorder))
+            {
+                return false;
+            }
+            else
+            {
+                Disorders.Add(newDisorder);
+                return true;
+            }
+
+
+
+        }
+
+        public static bool isHealthy()
+        {
+            if (Disorders.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static void Estado()
+        {
+            Console.WriteLine("{0} {1} tiene los siguientes desordenes:", nombre, apellidos);
+
+            for (int i = 0; i < Disorders.Count; i++)
+            {
+                Console.WriteLine($"{i}. {Disorders[i]}");
+            }
+        }
+
+    }
+}
+    
+
--- PatientProyect/PatientProyect/Program.cs
+++ PatientProyect/PatientProyect/Program.cs
@@ -3,109 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PatientProyect;
 
 namespace PatientProyect
 {
     class Program
     {
-        public static string nombre = "Fulano";
-        public static string apellidos = "Mengano";
-        public static List<string> Disorders = new List<string>();
-        public static string[] enfermedades = {"Alcohol Addiction", "Anorexia", "Antisocial", "Anxiety", "Bipolar", "Bulimia", "Dependent (lack of self" +
-                    "confidence" , "Depression", "Kleptomaniac", "Nicotine addiction", "Obsessive-compulsive", "Panic", "Paranoid", "Post-traumatic stress",
-            "Programmer", "Tester",};
-
-        static string DisorderGet(uint x)
-        {
-            for (int i = 0; i < enfermedades.Length; i++)
-            {
-                long shift = 1 << i;
-                if ((x & shift) > 0)
-                {
-                    return enfermedades[i];
-                }
-
-            }
-            return "";
-        }
-        static bool hasDisorder(uint disorder)
-        {
-            string checkDisorder = DisorderGet(disorder);
-
-            if (Disorders.Contains(checkDisorder))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        static bool addDisorder(uint x)
-        {
-            string newDisorder = DisorderGet(x);
-
-            if (newDisorder == "")
-            {
-                return false;
-            }
-
-            else if (Disorders.Contains(newDisorder))
-            {
-                return false;
-            }
-            else
-            {
-                Disorders.Add(newDisorder);
-                return true;
-            }
-
-
-
-        }
-
-        static bool isHealthy()
-        {
-            if (Disorders.Count == 0)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        static void Estado()
-        {
-            Console.WriteLine("{0} {1} tiene los siguientes desordenes:", nombre, apellidos);
-
-            for (int i = 0; i < Disorders.Count; i++)
-            {
-                Console.WriteLine($"{i}. {Disorders[i]}");
-            }
-        }
         static void Main(string[] args)
         {
+            Patient.addDisorder(1);
+            Patient.addDisorder(8);
 
-
-            addDisorder(1);
-            addDisorder(8);
-
-            Console.WriteLine(hasDisorder(16));
-            for (int i = 0; i < Disorders.Count; i++)
+            Console.WriteLine(Patient.hasDisorder(16));
+            for (int i = 0; i < Patient.Disorders.Count; i++)
             {
-                Console.WriteLine(Disorders[i]);
+                Console.WriteLine(Patient.Disorders[i]);
             }
 
-            Console.WriteLine(isHealthy());
+            Console.WriteLine(Patient.isHealthy());
 
-            Estado();
+            Patient.Estado();
 
 
             Console.ReadKey();
-
         }
     }
 }
-
